9073997fe353ca6e4cb6e991bd61c84106d00912
Daemon Targaryen <131575410+bharath6300000@users.noreply.github.com>
2023-07-08 23:20:57 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_vicky-user
-add_lunch_combo omni_vicky-userdebug
-add_lunch_combo omni_vicky-eng
+add_lunch_combo twrp_vicky-user
+add_lunch_combo twrp_vicky-userdebug
+add_lunch_combo twrp_vicky-eng
