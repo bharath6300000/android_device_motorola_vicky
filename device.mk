126ec4cacb6cb8542999cf6e8b567fe9f0011846
Daemon Targaryen <131575410+bharath6300000@users.noreply.github.com>
2023-07-08 21:28:51 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -16,10 +16,12 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-impl.recovery \
     android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
     bootctrl.mt6789
+    bootctrl.mt6893.recovery
 
 PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.mt6789 \
